d3e911d5df9d1b90542445bb0b60d8d406fa77d0
bhuvnesh06 <rajpt77@gmail.com>
2026-04-10 22:24:11 +0530

    changes

--- app.py
+++ app.py
@@ -9,9 +9,5 @@ def home():
     return render_template("index.html")
 
  
-
-@app.route('/<folder>/<filename>')
-def serve_images(folder, filename):
-    return send_from_directory(os.path.join('templates', folder), filename)
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5173)  # 🔥 unique port
